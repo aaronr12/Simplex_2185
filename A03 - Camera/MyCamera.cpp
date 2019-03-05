b15c638e4bf99c284848808e802c364bf74ea34d
ajr8498 <ajr8498@rit.edu>
2019-03-04 19:33:29 -0500

    A03 Deliverable

--- A03 - Camera/MyCamera.cpp
+++ A03 - Camera/MyCamera.cpp
@@ -153,14 +153,21 @@ void Simplex::MyCamera::CalculateProjectionMatrix(void)
 void MyCamera::MoveForward(float a_fDistance)
 {
 	//The following is just an example and does not take in account the forward vector (AKA view vector)
-	
+	/*
 	m_v3Position += vector3(0.0f, 0.0f,-a_fDistance);
 	m_v3Target += vector3(0.0f, 0.0f, -a_fDistance);
-	m_v3Above += vector3(0.0f, 0.0f, -a_fDistance);	
+	m_v3Above += vector3(0.0f, 0.0f, -a_fDistance);
+	*/
+
+	m_v3Position += -a_fDistance*m_v3Forward;
+	m_v3Above += -a_fDistance * m_v3Forward;
+	m_v3Target += -a_fDistance * m_v3Forward;
+	m_v3Forward = glm::normalize(m_v3Position - m_v3Target);
 }
 
 void MyCamera::MoveHorizontal(float a_fDistance)
 {
+	// allow the camera to rotate horizontally
 	quaternion Quat = glm::angleAxis(glm::radians(a_fDistance), m_v3Above - m_v3Position);
 	m_v3Above = Quat * (m_v3Above - m_v3Position) + m_v3Position;
 	m_v3Target = Quat * (m_v3Target - m_v3Position) + m_v3Position;
@@ -170,6 +177,7 @@ void MyCamera::MoveHorizontal(float a_fDistance)
 
 void MyCamera::MoveVertical(float a_fDistance)
 {
+	// allow the camera to rotate vertically
 	quaternion Quat = glm::angleAxis(glm::radians(a_fDistance), m_v3Right);
 	m_v3Above = Quat * (m_v3Above - m_v3Position) + m_v3Position;
 	m_v3Target = Quat * (m_v3Target - m_v3Position) + m_v3Position;
@@ -178,8 +186,15 @@ void MyCamera::MoveVertical(float a_fDistance)
 }
 void MyCamera::MoveSideways(float a_fDistance)
 {
+	/*
 	// moves sideways without taking view vector into account
 	m_v3Position += vector3(a_fDistance, 0.0f, 0.0f);
 	m_v3Target += vector3(a_fDistance, 0.0f, 0.0f);
 	m_v3Above += vector3(a_fDistance, 0.0f, 0.0f);
+	*/
+
+	m_v3Position += a_fDistance * m_v3Right;
+	m_v3Above += a_fDistance * m_v3Right;
+	m_v3Target += a_fDistance * m_v3Right;
+	m_v3Forward = glm::normalize(m_v3Position - m_v3Target);
 }

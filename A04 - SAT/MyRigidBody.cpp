b34b7e8ed5901f2e929d3a7dbc453a30f69d9b04
aaronr12 <ajr8498@g.rit.edu>
2019-03-24 23:02:04 -0400

    A04 updated

--- A04 - SAT/MyRigidBody.cpp
+++ A04 - SAT/MyRigidBody.cpp
@@ -292,12 +292,12 @@ uint MyRigidBody::SAT(MyRigidBody* const a_pOther)
 	// Compute rotation matrix expressing b in a's coordinate frame
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < 3; j++)
-			R[i][j] = Dot(a.u[i], b.u[j]);
+			R[i][j] = Dot(a.m_m4ToWorld[i], b.m_m4ToWorld[j]);
 
 	// Compute translation vector t
-	Vector t = b.c - a.c;
+	vector t = b.m_v3Center - a.m_v3Center;
 	// Bring translation into a's coordinate frame
-	t = Vector(Dot(t, a.u[0]), Dot(t, a.u[2]), Dot(t, a.u[2]));
+	t = Vector(Dot(t, a.m_m4ToWorld[0]), Dot(t, a.m_m4ToWorld[2]), Dot(t, a.m_m4ToWorld[2]));
 
 	// Compute common subexpressions. Add in an epsilon term to
 	// counteract arithmetic errors when two edges are parallel and
@@ -308,62 +308,62 @@ uint MyRigidBody::SAT(MyRigidBody* const a_pOther)
 
 	// Test axes L = A0, L = A1, L = A2
 	for (int i = 0; i < 3; i++) {
-		ra = a.e[i];
-		rb = b.e[0] * AbsR[i][0] + b.e[1] * AbsR[i][1] + b.e[2] * AbsR[i][2];
+		ra = a.m_v3HalfWidth[i];
+		rb = b.m_v3HalfWidth[0] * AbsR[i][0] + b.m_v3HalfWidth[1] * AbsR[i][1] + b.m_v3HalfWidth[2] * AbsR[i][2];
 		if (Abs(t[i]) > ra + rb) return 0;
 	}
 
 	// Test axes L = B0, L = B1, L = B2
 	for (int i = 0; i < 3; i++) {
-		ra = a.e[0] * AbsR[0][i] + a.e[1] * AbsR[1][i] + a.e[2] * AbsR[2][i];
-		rb = b.e[i];
+		ra = a.m_v3HalfWidth[0] * AbsR[0][i] + a.m_v3HalfWidth[1] * AbsR[1][i] + a.m_v3HalfWidth[2] * AbsR[2][i];
+		rb = b.m_v3HalfWidth[i];
 		if (Abs(t[0] * R[0][i] + t[1] * R[1][i] + t[2] * R[2][i]) > ra + rb) return 0;
 	}
 
 	// Test axis L = A0 x B0
-	ra = a.e[1] * AbsR[2][0] + a.e[2] * AbsR[1][0];
-	rb = b.e[1] * AbsR[0][2] + b.e[2] * AbsR[0][1];
+	ra = a.m_v3HalfWidth[1] * AbsR[2][0] + a.m_v3HalfWidth[2] * AbsR[1][0];
+	rb = b.m_v3HalfWidth[1] * AbsR[0][2] + b.m_v3HalfWidth[2] * AbsR[0][1];
 	if (Abs(t[2] * R[1][0] - t[1] * R[2][0]) > ra + rb) return 0;
 
 	// Test axis L = A0 x B1
-	ra = a.e[1] * AbsR[2][1] + a.e[2] * AbsR[1][1];
-	rb = b.e[0] * AbsR[0][2] + b.e[2] * AbsR[0][0];
+	ra = a.m_v3HalfWidth[1] * AbsR[2][1] + a.m_v3HalfWidth[2] * AbsR[1][1];
+	rb = b.m_v3HalfWidth[0] * AbsR[0][2] + b.m_v3HalfWidth[2] * AbsR[0][0];
 	if (Abs(t[2] * R[1][1] - t[1] * R[2][1]) > ra + rb) return 0;
 
 	// Test axis L = A0 x B2
-	ra = a.e[1] * AbsR[2][2] + a.e[2] * AbsR[1][2];
-	rb = b.e[0] * AbsR[0][1] + b.e[1] * AbsR[0][0];
+	ra = a.m_v3HalfWidth[1] * AbsR[2][2] + a.m_v3HalfWidth[2] * AbsR[1][2];
+	rb = b.m_v3HalfWidth[0] * AbsR[0][1] + b.m_v3HalfWidth[1] * AbsR[0][0];
 	if (Abs(t[2] * R[1][2] - t[1] * R[2][2]) > ra + rb) return 0;
 
 	// Test axis L = A1 x B0
-	ra = a.e[0] * AbsR[2][0] + a.e[2] * AbsR[0][0];
-	rb = b.e[1] * AbsR[1][2] + b.e[2] * AbsR[1][1];
+	ra = a.m_v3HalfWidth[0] * AbsR[2][0] + a.m_v3HalfWidth[2] * AbsR[0][0];
+	rb = b.m_v3HalfWidth[1] * AbsR[1][2] + b.m_v3HalfWidth[2] * AbsR[1][1];
 
 	if (Abs(t[0] * R[2][0] - t[2] * R[0][0]) > ra + rb) return 0;
 
 	// Test axis L = A1 x B1
-	ra = a.e[0] * AbsR[2][1] + a.e[2] * AbsR[0][1];
-	rb = b.e[0] * AbsR[1][2] + b.e[2] * AbsR[1][0];
+	ra = a.m_v3HalfWidth[0] * AbsR[2][1] + a.m_v3HalfWidth[2] * AbsR[0][1];
+	rb = b.m_v3HalfWidth[0] * AbsR[1][2] + b.m_v3HalfWidth[2] * AbsR[1][0];
 	if (Abs(t[0] * R[2][1] - t[2] * R[0][1]) > ra + rb) return 0;
 
 	// Test axis L = A1 x B2
-	ra = a.e[0] * AbsR[2][2] + a.e[2] * AbsR[0][2];
-	rb = b.e[0] * AbsR[1][1] + b.e[1] * AbsR[1][0];
+	ra = a.m_v3HalfWidth[0] * AbsR[2][2] + a.m_v3HalfWidth[2] * AbsR[0][2];
+	rb = b.m_v3HalfWidth[0] * AbsR[1][1] + b.m_v3HalfWidth[1] * AbsR[1][0];
 	if (Abs(t[0] * R[2][2] - t[2] * R[0][2]) > ra + rb) return 0;
 
 	// Test axis L = A2 x B0
-	ra = a.e[0] * AbsR[1][0] + a.e[1] * AbsR[0][0];
-	rb = b.e[1] * AbsR[2][2] + b.e[2] * AbsR[2][1];
+	ra = a.m_v3HalfWidth[0] * AbsR[1][0] + a.m_v3HalfWidth[1] * AbsR[0][0];
+	rb = b.m_v3HalfWidth[1] * AbsR[2][2] + b.m_v3HalfWidth[2] * AbsR[2][1];
 	if (Abs(t[1] * R[0][0] - t[0] * R[1][0]) > ra + rb) return 0;
 
 	// Test axis L = A2 x B1
-	ra = a.e[0] * AbsR[1][1] + a.e[1] * AbsR[0][1];
-	rb = b.e[0] * AbsR[2][2] + b.e[2] * AbsR[2][0];
+	ra = a.m_v3HalfWidth[0] * AbsR[1][1] + a.m_v3HalfWidth[1] * AbsR[0][1];
+	rb = b.m_v3HalfWidth[0] * AbsR[2][2] + b.m_v3HalfWidth[2] * AbsR[2][0];
 	if (Abs(t[1] * R[0][1] - t[0] * R[1][1]) > ra + rb) return 0;
 
 	// Test axis L = A2 x B2
-	ra = a.e[0] * AbsR[1][2] + a.e[1] * AbsR[0][2];
-	rb = b.e[0] * AbsR[2][1] + b.e[1] * AbsR[2][0];
+	ra = a.m_v3HalfWidth[0] * AbsR[1][2] + a.m_v3HalfWidth[1] * AbsR[0][2];
+	rb = b.m_v3HalfWidth[0] * AbsR[2][1] + b.m_v3HalfWidth[1] * AbsR[2][0];
 	if (Abs(t[1] * R[0][2] - t[0] * R[1][2]) > ra + rb) return 0;
 
 	// Since no separating axis is found, the OBBs must be intersecting
